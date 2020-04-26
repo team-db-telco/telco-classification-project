4628cc51d489df498b5a31bc751417bc08369e8e
David Wederstrandt Sr <david.wederstrandtsr@gmail.com>
2020-04-25 22:47:49 -0500

    updating explore files

--- explore.py
+++ explore.py
@@ -1,29 +1,21 @@
 import pandas as pd
 import numpy as np
 
-# If they pay $10.00 or less, they get ten_or_less
-# If they pay $20.00 or less, they get twenty
-# If they pay $30.00 or less, they get thirty
-# If they pay $40.00 or less, they get forty
-# If they pay $50.00 or less, they get fifty
-# If they pay $60.00 or less, they get sixty
-# If they pay $70.00 or less, they get seventy
-# If they pay $80.00 or less, they get eighty
-# If they pay $90.00 or less, they get ninety
-# If they pay $90.00 or more, they get more_then_nintey
-# else, they get zero, error check
-# def group_monthly_charges(df):
-#     df['monthly_charges_group'] = np.where( (df['monthly_charges'] <= 10.00), 'ten_or_less',
-#                                        np.where( (df['monthly_charges'] <= 20.00), 'twenty',
-#                                            np.where( (df['monthly_charges'] <= 30.00), 'thirty',
-#                                                np.where( (df['monthly_charges'] <= 40.00), 'forty',
-#                                                    np.where( (df['monthly_charges'] <= 50.00), 'fifty',
-#                                                         np.where( (df['monthly_charges'] <= 60.00), 'sixty',
-#                                                            np.where( (df['monthly_charges'] <= 70.00), 'seventy',
-#                                                                np.where( (df['monthly_charges'] <= 80.00), 'eighty',
-#                                                                    np.where( (df['monthly_charges'] <= 90.00), 'ninety',
-#                                                                        np.where( (df['monthly_charges'] > 90.00), 'more_then_nintey', 'zero'))))))))))
-#     return df
+# monthly charges set to groups by range
+def group_monthly_charges(df):
+    df['monthly_charges_group'] = np.where( (df['monthly_charges'] <= 20.00), 'one_to_twenty',
+                                        np.where( (df['monthly_charges'] <= 30.00), 'twenty_one_to_thirty',
+                                            np.where( (df['monthly_charges'] <= 40.00), 'thirty_one_to_forty',
+                                                np.where( (df['monthly_charges'] <= 50.00), 'forty_one_to_fifty',
+                                                     np.where( (df['monthly_charges'] <= 60.00), 'fifty_one_to_sixty',
+                                                         np.where( (df['monthly_charges'] <= 70.00), 'sixty_one_to_seventy',
+                                                            np.where( (df['monthly_charges'] <= 80.00), 'seventy_one_to_eighty',
+                                                                np.where( (df['monthly_charges'] <= 90.00), 'eighty_one_to_ninety',
+                                                                    np.where( (df['monthly_charges'] <= 100.00), 'ninety_one_to_one_hundred',
+                                                                        np.where( (df['monthly_charges'] <= 110.00), 'one_hundred_to_one_hundred_ten',
+                                                                            np.where( (df['monthly_charges'] <= 120.00), 'one_hundred_eleven_to_hundred_twenty',
+                                                                                np.where( (df['monthly_charges'] > 120.00), 'more_then_one_hundred_twenty', 'zero'))))))))))))
+    return df
 
 
 
@@ -45,10 +37,3 @@ def group_tenure(df):
     return df
 
 
-def group_monthly_charges(df):
-    df['monthly_charges_group'] = np.where( (df['monthly_charges'] <= 20.00), 'zero to twenty',
-                                        np.where( (df['monthly_charges'] <= 40.00), 'twenty_one to forty',
-                                                np.where( (df['monthly_charges'] <= 60.00), 'forty_one to sixty',
-                                                        np.where( (df['monthly_charges'] <= 80.00), 'sixty_one to eighty',
-                                                                    np.where( (df['monthly_charges'] > 80.00), 'more_then_eighty', 'zero')))))
-    return df
